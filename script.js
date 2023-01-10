488f897079cdec6c9436a77f88fdaa3fdf1f9dd5
davidecardelli <davide.cardelli.94@gmail.com>
2023-01-10 09:37:19 +0100

    stampato in pagina

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log('vue ok', Vue);
+
+const app = Vue.createApp({
+    data(){
+        return {
+            firstName: 'Davide',
+            lastName: 'Cardelli'
+        }
+    }
+})
+
+app.mount('#root')
